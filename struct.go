23cdf7aa991d348cd4214c3a7efb17edf72893e9
devin <24190720+lumaaaaaa@users.noreply.github.com>
2024-04-20 03:58:33 -0400

    Refactoring for clarity

---
+++ struct.go
@@ -0,0 +1,29 @@
+package main
+
+type RequestBody struct {
+	FunctionImageGen  bool `json:"function_image_gen"`
+	FunctionWebSearch bool `json:"function_web_search"`
+	MaxTokens         int  `json:"max_tokens"`
+	Messages          []struct {
+		Content string `json:"content"`
+		Role    string `json:"role"`
+	} `json:"messages"`
+	Model string `json:"model"`
+}
+
+type ResponseChunk struct {
+	Id                string      `json:"id"`
+	Object            string      `json:"object"`
+	Created           int         `json:"created"`
+	Model             string      `json:"model"`
+	SystemFingerprint interface{} `json:"system_fingerprint"`
+	Choices           []struct {
+		Index int `json:"index"`
+		Delta struct {
+			Role    string `json:"role"`
+			Content string `json:"content"`
+		} `json:"delta"`
+		Logprobs     interface{} `json:"logprobs"`
+		FinishReason interface{} `json:"finish_reason"`
+	} `json:"choices"`
+}
